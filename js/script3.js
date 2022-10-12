3dbe81869567802ae845b034c8b8467010cf53e4
Francescolapiana <Lushika1219@gmail.com>
2022-10-12 17:41:51 +0200

    Hotwell 2

---
+++ js/script3.js
@@ -0,0 +1,12 @@
+const num = [1, 2, 3, 4, 5, 6, 7]
+console.log(num);
+
+for (let i = 0; i < num.length; i++) {
+    if (i % 0 ){
+        console.log(i);
+        i= parseInt(i);
+        console.log(i);
+
+    }
+    
+}
