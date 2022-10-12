783f0ebc69139efbdcd0d16c547105b7850518c8
Francescolapiana <Lushika1219@gmail.com>
2022-10-12 16:59:10 +0200

    snack 1 fatto

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ const button = document.getElementById('btn1')
 let fx = function() {
     let number1 = document.getElementById('input1').value 
     console.log(number1);
+    number1 = parseInt(number1);
+    console.log(number1);
 
     if(number1 % 2 == 0){
         document.getElementById('stamp1').innerHTML = number1;
     }
     else{
-        let number1b = parseInt(number1 + 1);
+        let number1b = number1 + 1;
         document.getElementById('stamp1').innerHTML = number1b;
 
     }
