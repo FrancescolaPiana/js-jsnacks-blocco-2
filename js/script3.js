d4686e49b063660d86a416f5e3b64b9081c8633e
Francescolapiana <Lushika1219@gmail.com>
2022-10-12 17:58:02 +0200

    Snack 3

--- js/script3.js
+++ js/script3.js
@@ -1,12 +1,18 @@
 const num = [1, 2, 3, 4, 5, 6, 7]
 console.log(num);
+document.getElementById("array11").innerHTML = num;
+let somma = 0;
 
-for (let i = 0; i < num.length; i++) {
-    if (i % 0 ){
-        console.log(i);
-        i= parseInt(i);
+for (let i = 0; i <= num.length; i++) {
+    if (i % 2 != 0){
         console.log(i);
 
+        let moment = i;
+        console.log(moment);
+       
+        somma = somma + moment;
+        console.log(somma);
+        document.getElementById('somma').innerHTML = somma
     }
     
 }
